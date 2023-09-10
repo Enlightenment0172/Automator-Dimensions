52d3914fa739371f1e12c695478a14b4815b706d
Enlightenment <82910689+Enlightenment0172@users.noreply.github.com>
2023-09-10 11:19:19 -0500

    Update index.js

--- src/core/secret-formula/flow/index.js
+++ src/core/secret-formula/flow/index.js
@@ -1,1 +1,7 @@
+import { flowMilestones } from "./flow-milestones";
+import { flowUpgrades } from "./flow-upgrades";
 
+export const flux = {
+  upgrades: flowUpgrades,
+  milestones: flowMilestones
+};
