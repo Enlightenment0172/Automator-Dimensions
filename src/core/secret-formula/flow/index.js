5e729ab0859e7115ac434d773214333e864e99be
Enlightenment <82910689+Enlightenment0172@users.noreply.github.com>
2023-09-11 08:21:39 -0500

    Delete src/core/secret-formula/flow directory

--- src/core/secret-formula/flow/index.js
+++
@@ -1,7 +0,0 @@
-import { flowMilestones } from "./flow-milestones";
-import { flowUpgrades } from "./flow-upgrades";
-
-export const flux = {
-  upgrades: flowUpgrades,
-  milestones: flowMilestones
-};
